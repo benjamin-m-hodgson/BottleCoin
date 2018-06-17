1ce14f11857cb7590c2171a7097ecc004a959a48
Ben Hodgson <bmh43@duke.edu>
2018-06-17 10:47:35 -0400

    Imported SafeMath library to avoid overflow/underflow calculation erros

--- contracts/BottleCoin.sol
+++ contracts/BottleCoin.sol
@@ -18,7 +18,8 @@ contract owned {
     }
 }
 
-contract BottleCoin is owned{
+contract BottleCoin is owned {
+  using SafeMath for uint;
 
   // Mapping to store active bottles
   mapping(bytes32 => uint) public activeBottleIndex;
@@ -157,6 +158,41 @@ contract BottleCoin is owned{
   // delete the contract from the blockchain
   function kill() onlyOwner public{
       selfdestruct(owner);
+  }
+
+}
+
+/**
+ * @title SafeMath
+ * @dev Math operations with safety checks that throw on error
+ */
+library SafeMath {
+
+  function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+    if (a == 0) {
+      return 0;
+    }
+    uint256 c = a * b;
+    assert(c / a == b);
+    return c;
+  }
+
+  function div(uint256 a, uint256 b) internal pure returns (uint256) {
+    // assert(b > 0); // Solidity automatically throws when dividing by 0
+    uint256 c = a / b;
+    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+    return c;
+  }
+
+  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+    assert(b <= a);
+    return a - b;
+  }
+
+  function add(uint256 a, uint256 b) internal pure returns (uint256) {
+    uint256 c = a + b;
+    assert(c >= a);
+    return c;
   }
 
 }
